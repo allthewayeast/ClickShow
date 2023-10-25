4c01a58cf76dafe049469d33247d6aea27f30ebe
allthewayeast <yangmingzhen@outlook.com>
2023-10-25 22:49:13 +0800

    限制多进程运行

---
+++ ClickShow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ClickShow
+{
+    internal static class Program
+    {
+        static Mutex mutex;
+        static string mutexName = "{3032D362-2184-47B6-9B8D-17413D7DF138}";
+
+        [STAThread]
+        static void Main()
+        {
+            Program.mutex = new Mutex(true, mutexName, out var createNew);
+
+            if (createNew)
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
